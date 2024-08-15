b517c1a59907e58119db861acaea92527ab15ecd
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2024-08-15 20:37:25 -0300

    add GameLoop

---
+++ README.md
@@ -0,0 +1,13 @@
+# Um Simples Jogo de Tetris
+Este é o desenvolvimento de um simples jogo de Tetris, utilizando a linguagem de programação Java e a biblioteca gráfica Swing.
+
+Meu objetivo com este projeto é aprender sobre a criação de jogos e aprimorar minhas habilidades de programação.
+
+## Detalhes do Projeto
+- As dimensões padrões do jogo são 1280x720 pixels.
+- Game Loop com 60 FPS. Quer dizer que o jogo atualiza 60 vezes por segundo.
+
+## Estudos
+
+### Game Loop
+O Game Loop é um padrão de projeto que é utilizado para controlar o tempo de execução de um jogo. Ele é responsável por <b>atualizar</b> a lógica do jogo</b> e <b>renderizar os gráficos</b>.
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,13 +3,50 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.BLACK);
         this.setLayout(null);// Desabilita o layout padrão do JPanel
+    }
+
+    public void start() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        // Game loop
+        double dralInterval = 1000000000/FPS;
+        double delta = 0;// Armazena o tempo que falta para atualizar o jogo
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / dralInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    private void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 }
